c9a4543787fe3ce31cafdfe48c245971c3f74a40
Sanket <sanketdave79@techmentation.com>
2017-08-14 05:37:01 +0000

    creating model for task

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811013242) do
+ActiveRecord::Schema.define(version: 20170814053528) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
