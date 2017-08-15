ba1ce2c4732c8504b224e8517f336070c6e9468d
Sanket <sanketdave79@techmentation.com>
2017-08-15 04:39:40 +0000

    saving projects in sqlite , create method in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,15 @@
 class ProjectsController < ApplicationController
   def new
+    @projects = Project.new(params[:projects])
+  end
+  
+  def create
+    @projects = Project.new(params[:projects])
+    @projects.save
+    redirect_to @projects
+  end
+  
+  def show
+    @projects = Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'projects/new', to: 'projects#new'
-  get 'tasks/new', to: 'tasks#new'
+  get 'projects/new', to:'projects#new'
+  get 'projects/create', to:'projects#create'
+  get 'tasks/new', to:'tasks#new'
   resources :projects
+  resources :tasks
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
