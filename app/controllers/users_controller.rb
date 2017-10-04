49368dbe1f9bb597ad6f144054a791db8ed0f6a0
Sanket <sanketdave79@techmentation.com>
2017-10-04 21:48:44 +0000

    User form , fields and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
+  
+  def new
+    @users = User.new
+  end
+  
+  def show
+    @users = User.find(params[:id])
+  end
+  
+  def create
+    @users = User.new(users_params)
+    if @users.save
+      redirect_to @users
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def users_params
+      params.require(:user).permit(:name, :password)
+    end
 end
