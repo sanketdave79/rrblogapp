2f9c153cb1a9d76dca9658e13f4db3aff4927208
Sanket <sanketdave79@techmentation.com>
2017-10-05 02:19:32 +0000

    Message model and controller files ,

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :tasks
   resources :users
+  resources :messages
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
